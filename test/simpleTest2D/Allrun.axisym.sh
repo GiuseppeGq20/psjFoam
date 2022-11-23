be12c5d73c4de3ca0cc5b3f6013d6073d1bf8c24
GiuseppeGq20 <71777513+GiuseppeGq20@users.noreply.github.com>
2022-11-23 12:07:43 +0100

    mod SimpleTest2D axisym case
    - updated fvSolution to operate in Pimple mode (at least 2 outer
      correction loops) to account for thermo properties changes in the
      timestep
    - updated sigma boundary conditions and dictionaries entries to use
      sigmaClass
    - updated T and V boundary conditions to use the order of magnitude of
      the actual simulation
    - updated blockMeshdict to construct a smaller mesh in dimensions,
      consistent with the actual simualtion
    - updated controlDict to use a timestep consistent with actual simulation
    - add Allrun script

---
+++ test/simpleTest2D/Allrun.axisym.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd "${0%/*}" || exit                                # Run from this directory
+. ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions        # Tutorial run functions
+#------------------------------------------------------------------------------
+
+#restore0Dir
+rm log.*
+rm -r 0 
+cp -r 0.orig.axisym 0
+runApplication foamListTimes -rm
+
+runApplication blockMesh -dict system/blockMeshDict.axisym
+
+runApplication extrudeMesh
+
+runApplication setFields
+
+runApplication $(getApplication)
+
+#------------------------------------------------------------------------------
