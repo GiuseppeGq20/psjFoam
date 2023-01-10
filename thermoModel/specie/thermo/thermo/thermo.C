9f1f8caedef99c1aa3f6c572b754baee54d9047f
GiuseppeGq20 <giuseppe.giaquinto22@gmail.com>
2023-01-10 16:54:21 +0100

    RESOLVED sim crash due to non conmverging NR algorithm
    - updated sigmoid treshold coefficients in thermoModel/specie/customModel
    that lead to numerical oscillation of the Newton Rapshon algorithm to
    calc T field from H field.
    - Increased maxIter and tolerance of the Newton Raphson algorithm
    - modified energyu equation source term to deactivate at first outer
      iteration of the pimple algorithm

--- thermoModel/specie/thermo/thermo/thermo.C
+++ thermoModel/specie/thermo/thermo/thermo.C
@@ -30,10 +30,10 @@ License
 /* * * * * * * * * * * * * * * private static data * * * * * * * * * * * * * */
 
 template<class Thermo, template<class> class Type>
-const Foam::scalar Foam::species::thermo<Thermo, Type>::tol_ = 1.0e-4;
+const Foam::scalar Foam::species::thermo<Thermo, Type>::tol_ = 1.0e-3;
 
 template<class Thermo, template<class> class Type>
-const int Foam::species::thermo<Thermo, Type>::maxIter_ = 1000;
+const int Foam::species::thermo<Thermo, Type>::maxIter_ = 10000;
 
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
