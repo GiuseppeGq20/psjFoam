f2c7239b24d03e3c33ee5a53bc7b38c1452c2956
GiuseppeGq20 <giuseppe.giaquinto22@gmail.com>
2023-02-07 11:55:53 +0100

    add cfMesh model Jet

---
+++ test/cfMeshJet/Allrun.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+cd "${0%/*}" || exit                                # Run from this directory
+. ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions        # Tutorial run functions
+#------------------------------------------------------------------------------
+
+#restore0Dir
+rm log.*
+rm -r 0 
+cp -r 0.orig.axisym 0
+runApplication foamListTimes -rm
+
+runApplication blockMesh
+
+#runApplication topoSet
+
+#refineMesh -dict system/refineMeshDict.cavity -overwrite > log.refineMesh
+
+#refineMesh   -overwrite >> log.refineMesh
+
+runApplication decomposePar -force
+
+runParallel snappyHexMesh
+
+#runParallel mirrorMesh -overwrite
+#runApplication refineMesh -overwrite
+
+#runApplication $(getApplication)
+
+#------------------------------------------------------------------------------
